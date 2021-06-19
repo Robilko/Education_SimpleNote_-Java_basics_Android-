75e07ae82802e3163d17362c83f2d9d054a76062
Robilko <sasha.robson@gmail.com>
2021-06-19 21:25:40 +0300

    Добавлено меню в action bar с двумя пунктами: "настройки" и "О приложении"

--- app/src/main/java/com/robivan/simplenote/MainActivity.java
+++ app/src/main/java/com/robivan/simplenote/MainActivity.java
@@ -1,10 +1,14 @@
 package com.robivan.simplenote;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NoteListFragment.Contract, EditNoteFragment.Contract {
     private static final String NOTES_LIST_FRAGMENT = "NOTES_LIST_FRAGMENT";
@@ -17,6 +21,25 @@ public class MainActivity extends AppCompatActivity implements NoteListFragment.
         setContentView(R.layout.activity_main);
         isTwoPanel = findViewById(R.id.second_fragment_container) != null;
         showNoteList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings_menu:
+                Toast.makeText(this, getResources().getString(R.string.settings_toast), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.about_app_menu:
+                Toast.makeText(this, getResources().getString(R.string.about_app_toast), Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void showNoteList() {
