6599df598d7226af6def0c7db58aae4dabe509f0
Robilko <sasha.robson@gmail.com>
2021-06-21 02:03:27 +0300

    Стандартный ActionBar заменен на ToolBar. Добавлено Popup menu к заметкам в списке Добавлено боковое меню (пока работает не правильно)

--- app/src/main/java/com/robivan/simplenote/NoteListFragment.java
+++ app/src/main/java/com/robivan/simplenote/NoteListFragment.java
@@ -1,15 +1,19 @@
 package com.robivan.simplenote;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.button.MaterialButton;
@@ -28,9 +32,9 @@ public class NoteListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_note_list, container, false);
         createNoteButton = view.findViewById(R.id.create_new_note);
         listLayout = view.findViewById(R.id.list_layout);
-
         return view;
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -68,7 +72,32 @@ public class NoteListFragment extends Fragment {
         for (NoteEntity note : notes) {
             Button button = new Button(getContext());
             button.setText(note.title);
-            button.setOnClickListener(v -> getContract().editNote(note));
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Activity activity = requireActivity();
+                    PopupMenu popupMenu = new PopupMenu(activity, v);
+                    activity.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            int id = item.getItemId();
+                            switch (id) {
+                                case R.id.edit_note_popup:
+                                    getContract().editNote(note);
+                                    return true;
+                                case R.id.add_note_to_favorite_popup:  //TODO реализовать добавление заметки в избранное
+                                case R.id.delete_popup:                //TODO реализовать удаление заметки
+                                    Toast.makeText(getContext(), getResources().getString(R.string.do_not_realised_toast),
+                                            Toast.LENGTH_SHORT).show();
+                                    return true;
+                            }
+                            return true;
+                        }
+                    });
+                    popupMenu.show();
+                }
+            });
             listLayout.addView(button);
         }
     }
